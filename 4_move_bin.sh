e42aaf2e3b8719d222dff2426142c11ee23cfa75
snq <snq@snq-Precision-T1700.(none)>
2015-06-18 15:18:27 +0800

    for ui

--- 4_move_bin.sh
+++ 4_move_bin.sh
@@ -1,2 +1,2 @@
 cp  ./hardware/ti/wlan/mac80211/firmware/wl18xx-conf.bin  wl18xx-bin 
-cp  ./hardware/ti/wlan/mac80211/firmware/wl18xx-fw.bin    wl18xx-bin
+cp  ./hardware/ti/wlan/mac80211/firmware/wl18xx-fw-mc.bin    wl18xx-bin
---
+++ 6_mk_system_file.sh
@@ -0,0 +1,4 @@
+rm -rf   out/target/product/beagleboneblack/data/misc/wlan
+mkdir  out/target/product/beagleboneblack/data/misc/wlan
+rm -rf  out/target/product/beagleboneblack/data/misc/wlan/fwpath
+cp  fwpath  out/target/product/beagleboneblack/data/misc/wlan/
