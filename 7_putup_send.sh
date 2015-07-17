323e32437752ad43f481c306144410d0f7045d2f
root <root@ubuntu.(none)>
2015-07-17 18:16:10 +0800

    update first boot .shell

--- 7_putup_send.sh
+++ 7_putup_send.sh
@@ -1,3 +1,3 @@
-tar zcvf image_folder.tar.gz  image_folder
-scp  image_folder.tar.gz  ubuntu@192.168.183.129:/home/ubuntu/
+tar zcvf image_folder_2.tar.gz  image_folder_2
+scp  image_folder_2.tar.gz  ubuntu@192.168.183.129:/home/ubuntu/
 
---
+++ image_folder_2/9_first_start_boot.sh
@@ -0,0 +1,4 @@
+cp ../u-boot/u-boot.img      first_start
+cp ../u-boot/spl/u-boot-spl.bin  first_start
+
+
